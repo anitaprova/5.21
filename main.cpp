f4773e016decc0bf670cde01b6f0f824ea25767e
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-12-13 20:02:28 -0500

    done

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+string reverse(string str){
+	if(str.length() == 0) {
+		return "";
+	}
+	
+	return reverse(str.substr(1)) + str.substr(0,1);
+}
+
+int main(){
+	cout << reverse("flow") << endl;
+	
+	return 0;
+}
